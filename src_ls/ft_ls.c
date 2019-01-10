abb7276eccccc919de629e16940d4d41fcd60e71
vsaltel <saltel.valentin@gmail.com>
2019-01-10 14:24:41 +0100

    add all

---
+++ src_ls/ft_ls.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsaltel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/04 13:20:58 by vsaltel           #+#    #+#             */
+/*   Updated: 2019/01/10 13:15:43 by vsaltel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include_ls/ft_ls.h"
+
+int		nb_file(t_folder *pfolder)
+{
+	DIR	*dirp;
+	struct dirent *dirc;
+	int i;
+
+	i = 0;
+	if ((dirp = opendir(pfolder->path)) != NULL)
+	{
+		while ((dirc = readdir(dirp)) != NULL)
+			i++;
+	}
+	pfolder->nb_file = i;
+	if (closedir(dirp) != 0)
+	{
+		perror("closedir :");
+		exit(-1);
+	}
+	return (i);
+}
+
+void	tmp(char *path)
+{
+	printf("recursive, path = %s\n", path);
+}
+
+t_folder	*recursive_option(t_folder *pfolder, t_option option)
+{
+	struct stat pstat;
+	char buf[30];
+	t_folder *new;
+	int i;
+
+	i = 0;
+	new = NULL;
+	while (pfolder->file[i].name != 0)
+	{
+		if (stat(str_pathfile(buf, pfolder->path, pfolder->file[i].name), &pstat) == -1)
+		{
+			perror("stat");
+			exit(-1);
+		}
+		else if (S_ISDIR(pstat.st_mode) && pfolder->file[i].name[0] != '.')
+		{
+			if (!(new = malloc(sizeof(t_folder))))
+				exit(-1);
+			new->path = ft_strdup(buf);
+			new->next = pfolder->next;
+			pfolder->next = new;
+			pfolder = new;
+			select_dir(new, option);
+			//tmp(buf);
+		}
+		i++;
+	}
+	return (new);
+}
+
+void	select_dir(t_folder *pfolder, t_option option)
+{
+	struct dirent *dirc;
+	int i;
+
+	pfolder->dirp = NULL;
+	if ((pfolder->dirp = opendir(pfolder->path)) != NULL)
+	{
+		nb_file(pfolder);
+		if (!(pfolder->file = malloc(sizeof(t_file) * (pfolder->nb_file + 1))))
+			exit(-1);
+		pfolder->file[pfolder->nb_file].name = 0;
+		i = 0;
+		while ((dirc = readdir(pfolder->dirp)) != NULL)
+			pfolder->file[i++].name = dirc->d_name;
+		if (option.R)
+			recursive_option(pfolder, option);		
+	}
+}
---
+++ src_ls/utils.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsaltel <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/07 13:07:22 by vsaltel           #+#    #+#             */
+/*   Updated: 2019/01/10 13:09:50 by vsaltel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include_ls/ft_ls.h"
+
+void	memset_option(t_option *option)
+{
+	option->l = 0;
+	option->R = 0;
+	option->a = 0;
+	option->r = 0;
+	option->t = 0;
+}
+
+void	memset_file(t_file *pfile)
+{
+	pfile->name = NULL;
+	pfile->mode = NULL;
+	pfile->nb_links = NULL;
+	pfile->owner = NULL;
+	pfile->group = NULL;
+	pfile->bytes = NULL;
+	pfile->date = NULL;
+}
+
+char	*str_pathfile(char *dst, const char *s1, const char *s2)
+{
+	int i;
+
+	i = 0;
+	while (*s1)
+		dst[i++] = *s1++;
+	if (dst[i - 1] != '/')
+		dst[i++] = '/';
+	while (*s2)
+		dst[i++] = *s2++;
+	dst[i] = '\0';
+	return (dst);
+}
+
+t_folder	*closealldir(t_folder *pfolder)
+{
+	t_folder *begin;
+
+	begin = pfolder;
+	while (pfolder)
+	{
+		if (closedir(pfolder->dirp) != 0)
+		{
+			perror("closedir :");
+			exit(-1);
+		}
+		pfolder = pfolder->next;
+	}
+	return (begin);
+}
