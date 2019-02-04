fafa72a4d9db811d9c4b14fa563fd5ad3f66959d
vsaltel <saltel.valentin@gmail.com>
2019-02-04 17:19:49 +0100

    total -l, minor major

--- src/parser.c
+++ src/parser.c
@@ -6,29 +6,55 @@
 /*   By: vsaltel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/03 15:24:49 by vsaltel           #+#    #+#             */
-/*   Updated: 2019/02/01 14:10:17 by vsaltel          ###   ########.fr       */
+/*   Updated: 2019/02/04 17:18:57 by vsaltel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_ls.h"
 
+t_folder	*malloc_pfolder(char *path)
+{
+	t_folder	*pfolder;
+
+	if (!(pfolder = malloc(sizeof(t_folder))))
+		exit(-1);
+	if (path)
+		pfolder->path = ft_strdup(path);
+	else
+	{
+		if (!(pfolder->path = malloc(sizeof(char) * 2)))
+			exit(-1);
+		pfolder->path[0] = '.';
+		pfolder->path[1] = '\0';
+	}
+	pfolder->next = NULL;
+	pfolder->total_blocks = 0;
+	pfolder->file = NULL;
+	return (pfolder);
+}
+
 void	set_option(t_option *option, char *str)
 {
 	int i;
 
-	i = 0;
+	i = 1;
 	while (str[i])
 	{
 		if (str[i] == 'l')
 			option->l = 1;
-		if (str[i] == 'R')
+		else if (str[i] == 'R')
 			option->R = 1;
-		if (str[i] == 'a')
+		else if (str[i] == 'a')
 			option->a = 1;
-		if (str[i] == 'r')
+		else if (str[i] == 'r')
 			option->r = 1;
-		if (str[i] == 't')
+		else if (str[i] == 't')
 			option->t = 1;
+		else
+		{
+			printf("usage : ft_ls [-lRart] [file ...]\n");
+			exit(-1);
+		}
 		i++;
 	}
 }
@@ -48,33 +74,18 @@ t_folder	*parse_options(t_folder *pfolder, t_option *option, int argc, char **ar
 	}
 	if (argc > 0)
 	{
-		if (!(pfolder = malloc(sizeof(t_folder))))
-			exit(-1);
-		pfolder->next = NULL;
-		pfolder->file = NULL;
-		pfolder->path = ft_strdup(*argv++);
+		pfolder = malloc_pfolder(*argv++);
 		begin = pfolder;
 		while (--argc > 0)
 		{
-			if (!(pfolder->next = malloc(sizeof(t_folder))))
-				exit(-1);
+			pfolder->next = malloc_pfolder(*argv++);
 			pfolder = pfolder->next;
-			pfolder->path = ft_strdup(*argv++);
-			pfolder->next = NULL;
-			pfolder->file = NULL;
 		}
 	}
 	else
 	{
-		if (!(pfolder = malloc(sizeof(t_folder))))
-			exit(-1);
-		if (!(pfolder->path = malloc(sizeof(char) * 2)))
-			exit(-1);
+		pfolder = malloc_pfolder(NULL);
 		begin = pfolder;
-		pfolder->path[0] = '.';
-		pfolder->path[1] = '\0';
-		pfolder->file = NULL;
-		pfolder->next = NULL;
 	}
 	return (begin);
 }
