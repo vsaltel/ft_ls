869deaafea1b698189ba2f096223107f5d2c29c4
Valentin <saltel.valentin@gmail.com>
2019-02-16 20:06:54 +0100

    leak acl

--- src/l_option.c
+++ src/l_option.c
@@ -6,7 +6,7 @@
 /*   By: frossiny <frossiny@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/24 18:09:50 by vsaltel           #+#    #+#             */
-/*   Updated: 2019/02/16 19:52:42 by vsaltel          ###   ########.fr       */
+/*   Updated: 2019/02/16 20:05:29 by vsaltel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,11 +93,13 @@ static void	fill_mode(t_file *pfile, struct stat pstat)
 
 void		ell_option(t_folder *pfolder, t_file *pfile, t_option option)
 {
+	acl_t	tmp;
 	fill_mode(pfile, pfile->pstat);
 	fill_other(pfile, pfile->pstat);
 	pfolder->total_blocks += pfile->pstat.st_blocks;
-	if (acl_get_file(pfile->path, ACL_TYPE_EXTENDED))
+	if ((tmp = acl_get_file(pfile->path, ACL_TYPE_EXTENDED)))
 			pfile->extand_perm = '+';
+	acl_free(tmp);
 	if (listxattr(pfile->path, NULL, 0, XATTR_NOFOLLOW) > 0)
 			pfile->extand_perm = '@';
 	if (pfile->mode[0] == 'l')
